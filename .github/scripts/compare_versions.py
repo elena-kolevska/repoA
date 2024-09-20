29bf091bafdf8c786238933eacabe21cf27b4393
Elena Kolevska <elena@kolevska.com>
2024-09-20 14:35:04 +0100

    update after code review
    Signed-off-by: Elena Kolevska <elena@kolevska.com>

--- .github/scripts/compare_versions.py
+++ .github/scripts/compare_versions.py
@@ -14,30 +14,34 @@
 # This script parses release version from Git tag and set the parsed version to
 # environment variable, REL_VERSION. If the tag is the final version, it sets
 # LATEST_RELEASE to true to add 'latest' tag to docker image.
- 
+
 import os
 import sys
 from packaging.version import Version, InvalidVersion
 
-def compare_versions(new_version, existing_version):
+# compare_versions returns True if the comparison is successful.
+# It returns False if the versions are invalid.
+# The result of the comparison is written to the VERSION_UPDATE_REQUIRED GitHub environment variable.
+def compare_versions(new_version, existing_version) -> bool:
     try:
         new_ver = Version(new_version)
         existing_ver = Version(existing_version)
     except InvalidVersion:
         print("Invalid version format")
-        sys.exit(1)
+        return False
 
-    if new_ver > existing_ver:
-        print("New version is greater than existing version.")
-        with open(os.getenv("GITHUB_ENV"), "a") as github_env:
-            github_env.write("UPDATE_REQUIRED=true\n")
-        
-    else:
-        print("New version is not greater. Skipping update.")
-        with open(os.getenv("GITHUB_ENV"), "a") as github_env:
-            github_env.write("UPDATE_REQUIRED=false\n")
-    sys.exit(0)
-    
+    update_required = new_ver > existing_ver
+    status_message = "New version is greater than existing version." if update_required else "New version is not greater. Skipping update."
+    print(status_message)
+
+    # Write the update requirement status to GITHUB_ENV
+    github_env_path = os.getenv("GITHUB_ENV")
+    if github_env_path:
+        with open(github_env_path, "a") as github_env:
+            github_env.write(f"VERSION_UPDATE_REQUIRED={str(update_required).lower()}\n")
+
+    return True
+
 if len(sys.argv) != 3:
     print("Usage: compare_versions.py <new_version> <existing_version>")
     sys.exit(1)
@@ -45,4 +49,8 @@ if len(sys.argv) != 3:
 new_version = sys.argv[1]
 existing_version = sys.argv[2]
 
-compare_versions(new_version, existing_version)
+if compare_versions(new_version, existing_version):
+    sys.exit(0)
+else:
+    sys.exit(1)
+
